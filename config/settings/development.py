ff593cb8045d462116f68e102ec481b654a5ee2c
Yousef <yosef.ghasemi.all@gmail.com>
2026-08-25 12:01:56 +0330

    chore(quality): add ruff for linting and formatting

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,3 +1,3 @@
-from .base import *
+from .base import *  # noqa: F403
 
-DEBUG = env.bool("DEBUG", default=True)
+DEBUG = env.bool("DEBUG", default=True)  # noqa: F405
--- config/settings/production.py
+++ config/settings/production.py
@@ -1,4 +1,4 @@
-from .base import *
+from .base import *  # noqa: F403
 
-DEBUG = env.bool("DEBUG", default=False)
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+DEBUG = env.bool("DEBUG", default=False)  # noqa: F405
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")  # noqa: F405
